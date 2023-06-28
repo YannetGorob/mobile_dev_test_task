91f0c247fe3ad066926fe3bfd36e74a2cee90294
Yana Horobchuk <78377338+YannetGorob@users.noreply.github.com>
2023-06-28 21:43:08 +0300

    feat: implement BaseNetwork

---
+++ lib/core/api/base/base_network.dart
@@ -0,0 +1,62 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:mobile_dev_test_task/core/api/base/api_result.dart';
+
+class BaseNetwork {
+  BaseNetwork(this._dio);
+
+  static const String _unexpectedErrorMessage = 'Unexpected error';
+  final Dio _dio;
+
+  Future<ApiResult<T>> sendRequest<T>({
+    required String path,
+    Map<String, dynamic>? queryParameters,
+    T Function(Map<String, dynamic> json)? parseResponse,
+    T Function(List<dynamic> json)? parseListResponse,
+  }) async {
+    if (parseResponse == null && parseListResponse == null && T != Null) {
+      throw ArgumentError();
+    }
+
+    try {
+      final result = await _dio.request<dynamic>(
+        path,
+        queryParameters: queryParameters,
+      );
+
+      final data = result.data;
+
+      if (kDebugMode) {
+        print(data);
+      }
+
+      if (result.statusCode == HttpStatus.ok) {
+        if (data is Map<String, dynamic> && parseResponse != null) {
+          final value = parseResponse(data);
+          return ApiResult<T>.success(value);
+        } else if (data is List && parseListResponse != null) {
+          final value = parseListResponse(data);
+          return ApiResult<T>.success(value);
+        } else {
+          return ApiResult<T>.failure(_unexpectedErrorMessage);
+        }
+      } else if (data is Map) {
+        final error = data['error'];
+        if (error is String) {
+          return ApiResult<T>.failure(error);
+        } else {
+          return ApiResult<T>.failure(_unexpectedErrorMessage);
+        }
+      } else {
+        return ApiResult<T>.failure(_unexpectedErrorMessage);
+      }
+    } catch (error, stacktrace) {
+      if (kDebugMode) {
+        print('$error: {$stacktrace}');
+      }
+      return ApiResult<T>.failure(_unexpectedErrorMessage);
+    }
+  }
+}
