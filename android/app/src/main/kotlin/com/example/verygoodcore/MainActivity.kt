02421fddabe36eb6a8dedbad4ae510540b286b8b
Yana Horobchuk <78377338+YannetGorob@users.noreply.github.com>
2023-06-28 10:20:57 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.mobile_dev_test_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:mobile_dev_test_task/app/app.dart';
+import 'package:mobile_dev_test_task/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
